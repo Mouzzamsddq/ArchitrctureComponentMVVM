12a8ea14bbcf6700cdbe459db6efff10dffb9f7d
Mouzzam Siddiqui <mohazzamsddq07333@gmail.com>
2022-10-01 09:16:10 +0530

    added aplication clas

--- app/src/main/java/com/example/androidarchitecturemvvm/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/androidarchitecturemvvm/ui/gallery/GalleryFragment.kt
@@ -2,5 +2,7 @@ package com.example.androidarchitecturemvvm.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.example.androidarchitecturemvvm.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fgragment_gallery)
