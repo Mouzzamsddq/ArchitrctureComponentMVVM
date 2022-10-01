966532952deddf828d913f1abdc71cf93a1165c1
Mouzzam Siddiqui <mohazzamsddq07333@gmail.com>
2022-10-01 14:59:11 +0530

    #1 completed paging source and paging adapter

--- app/src/main/java/com/example/androidarchitecturemvvm/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/androidarchitecturemvvm/ui/gallery/GalleryFragment.kt
@@ -5,16 +5,33 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.androidarchitecturemvvm.R
+import com.example.androidarchitecturemvvm.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fgragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
     private val viewModel by viewModels<GalleryViewModel>()
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentGalleryBinding.bind(view)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        val adapter = UnsplashPhotoAdapter()
+        binding.apply {
+            unsplashImageRv.setHasFixedSize(true)
+            unsplashImageRv.adapter  = adapter
+
         }
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
